0934f24ccb720971b655a71885cab2ca0a7ab861
Xandrix Paguntalan <xandrixjill@gmail.com>
2022-03-08 19:19:28 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from Employee import Employee, Manager, Executive
+
+Employee = Employee("John Smith", 45000.00)
+Manager = Manager("Jane Doe", 60000.00, "Widgets")
+Executive = Executive("Weird Guy", 90000.00, "Thingies")
+
+print(Employee)
+print(Manager)
+print(Executive)
